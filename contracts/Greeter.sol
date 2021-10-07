70d599885e38b6741b758e7fb89dffcf30fe60a7
Milad Mehdizade <milad.mehdizade1394@gmail.com>
2021-10-07 11:14:04 +0330

    fix single qoute

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,13 +1,13 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity 0.7.0;
 
-import 'hardhat/console.sol';
+import "hardhat/console.sol";
 
 contract Greeter {
   string private greeting;
 
   constructor(string memory _greeting) {
-    console.log('Deploying a Greeter with greeting:', _greeting);
+    console.log("Deploying a Greeter with greeting:", _greeting);
     greeting = _greeting;
   }
 
--- contracts/VentIDO.sol
+++ contracts/VentIDO.sol
@@ -4,9 +4,7 @@ pragma solidity 0.8.4;
 import "hardhat/console.sol";
 
 contract VentIDO {
-    
   event LogDepositReceived(address);
-  bytes32 private a = keccak256('test');
 
   constructor(uint256 age) {
     console.log("Initialized with", age);
