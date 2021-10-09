d1bf27c291026d8cb415b467157be6e1e64cf228
Milad Mehdizade <milad.mehdizade1394@gmail.com>
2021-10-09 14:31:49 +0330

    change version

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity 0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "hardhat/console.sol";
 
@@ -10,7 +10,7 @@ contract RewardToken is ERC20 {
     string memory name,
     string memory symbol,
     uint256 initialSupply
-  ) public ERC20(name, symbol) {
+  )  ERC20(name, symbol) {
     _mint(msg.sender, initialSupply * 10**18);
   }
 }
