d9ceed4d1526d9d63637b8d897ebb222256a6168
Milad Mehdizade <milad.mehdizade1394@gmail.com>
2021-10-09 14:08:32 +0330

    fix access control fix stack too deep
    change dateTime type to uint256
    
    add test

--- contracts/VentIDO.sol
+++ contracts/VentIDO.sol
@@ -1,13 +1,13 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity 0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/security/PullPayment.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
@@ -15,17 +15,12 @@ import "./Whitelist.sol";
 
 import "hardhat/console.sol";
 
-contract VentIDO is Ownable, AccessControl, Pausable , Whitelist {
-
+contract VentIDO is Ownable, AccessControl, Pausable, Whitelist {
   using SafeERC20 for IERC20;
 
-
   // **** <Roles> ****
-  bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
-  bytes32 public constant POOL_OWNER_ROLE = keccak256("POOL_OWNER_ROLE");
-  bytes32 public constant INVESTOR_ROLE = keccak256("INVESTOR_ROLE");
+  bytes32 private constant POOL_OWNER_ROLE = keccak256("POOL_OWNER_ROLE");
   // **** </Roles> ****
-
 
   // **** <Structs> ****
   enum PoolStatus {
@@ -39,36 +34,30 @@ contract VentIDO is Ownable, AccessControl, Pausable , Whitelist {
   struct PoolInfo {
     uint256 hardCap; // how much project wants to raise
     uint256 softCap; //how much of the raise will be accepted as successful IDO
-    string startDateTime; //TODO how to represent timestamp?
-    string endDateTime;
+    uint256 startDateTime; //TODO how to represent timestamp?
+    uint256 endDateTime;
     address walletAddress; // address where Ether is sent
     IERC20 projectTokenAddress; //the address of the token that project is offering in return
     uint16 minAllocationPerUser;
     uint256 maxAllocationPerUser;
     PoolStatus status; //: by default “Upcoming”,
-
     uint256 totalTokenProvided; //TODO is it about allowance?
-    uint exchangeRate;          //TODO how to?
-    uint tokenPrice;            //TODO how to?
-    uint256 totalTokenSold;//TODO after implementing Deposit and Withdrawal
-
+    uint256 exchangeRate; //TODO how to?
+    uint256 tokenPrice; //TODO how to?
+    uint256 totalTokenSold; //TODO after implementing Deposit and Withdrawal
   }
 
   struct InvestorInfo {
     address userAddress;
     uint256 allocation;
-    mapping(address => uint256) rewardDebt; // Reward debt
   }
   // **** </Structs> ****
 
-
-
   // **** <State> ****
-  PoolInfo[] public poolInfo;
-  mapping(address => InvestorInfo) public investorInfo;
+  PoolInfo[] private poolInfo;
+
+  // mapping(address => uint256) public investorsParticipation; // users participation amount
   // **** </State> ****
-
-
 
   // **** <Events> ****
   event PoolCreated(address indexed poolOwner);
@@ -76,44 +65,45 @@ contract VentIDO is Ownable, AccessControl, Pausable , Whitelist {
   event RetrieveTokensFromPool(address indexed investor, uint256 amount);
   event RetrieveFundsFromPool(address indexed poolOwner, uint256 amount);
   event LogDepositReceived(address indexed from, bytes data);
+
   // **** </Events> ****
 
-
-
   constructor() {
-    _setupRole(ADMIN_ROLE, _msgSender());
+    _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
   }
-
-
 
   // **** <External> ****
-  function getPoolDetails() external view returns (PoolInfo memory) {
-      require(
-        address(poolInfo[0].walletAddress) == address(0), "Pool not created yet");
+  function getPoolDetails() external view {
+    // returns (PoolInfo memory)
+    require(
+      address(poolInfo[0].walletAddress) == address(0),
+      "Pool not created yet"
+    );
 
-        // Pool data that needs to be retrieved:
-        //   - Investors and their allocations //TODO after implementing Deposit
-        //   - Number of tokens sold           //TODO after implementing Deposit and Withdrawal
-        //   - Token price                     //TODO 
-        //   - Exchange rate                   //TODO
-        //   - Funding Target / hard cap
-        //   - Total tokens provided for the pool
-        //   - Max allocation per user
-        //   - Min allocation per user
-        //   - IDO start timestamp
-      // return {
-      //   min: 1
-      // };
-      return poolInfo[0];
+    // Pool data that needs to be retrieved:
+    //   - Investors and their allocations //TODO after implementing Deposit
+    //   - Number of tokens sold           //TODO after implementing Deposit and Withdrawal
+    //   - Token price                     //TODO
+    //   - Exchange rate                   //TODO
+    //   - Funding Target / hard cap
+    //   - Total tokens provided for the pool
+    //   - Max allocation per user
+    //   - Min allocation per user
+    //   - IDO start timestamp
+    // return {
+    //   min: 1
+    // };
+    // return poolInfo[0];
   }
 
-  function deposit() external payable onlyWhitelisted returns (bool) {
-
-    // check if pool is still open?
-    require(isPoolOpen(poolInfo[0]), "Pool not open!");
-
+  function deposit()
+    external
+    payable
+    onlyWhitelisted
+    pooIsOngoing
+    returns (bool)
+  {
     //TODO check if hard cap is reached
-    
     // TODO keep record of user participation
   }
 
@@ -126,56 +116,85 @@ contract VentIDO is Ownable, AccessControl, Pausable , Whitelist {
   receive() external payable {
     emit LogDepositReceived(_msgSender(), _msgData());
   }
+
   // **** </External> ****
 
-
-
   // **** <External-Admin> ****
-  function setPoolOwner(address poolOwnerAddress) external onlyOwner {
-    grantRole(POOL_OWNER_ROLE, poolOwnerAddress);
+  function grantPoolOwnerRole(address _ownerAddress) external onlyOwner {
+    grantRole(POOL_OWNER_ROLE, _ownerAddress);
+    console.log("new Pool owner", _ownerAddress);
   }
+
+  function revokePoolOwnerRole(address _ownerAddress) external onlyOwner {
+    revokeRole(POOL_OWNER_ROLE, _ownerAddress);
+    console.log("pool has no owner", _ownerAddress);
+  }
+
   // **** </External-Admin> ****
 
-
-  
   // **** <External-PoolOwner> ****
-  function createPool(PoolInfo memory pool) external onlyRole(POOL_OWNER_ROLE)
+  function createPool()
+    external
+    view
+    /*PoolInfo memory pool*/
+    onlyRole(POOL_OWNER_ROLE)
     returns (bool)
   {
-    poolInfo.push(
-      PoolInfo({
-        hardCap: pool.hardCap,
-        softCap: pool.softCap,
-        startDateTime: pool.startDateTime,
-        endDateTime: pool.endDateTime,
-        walletAddress: pool.walletAddress,
-        projectTokenAddress: pool.projectTokenAddress,
-        minAllocationPerUser: pool.minAllocationPerUser,
-        maxAllocationPerUser: pool.maxAllocationPerUser,
-        status: PoolStatus.Upcoming
-      })
-    );
+    console.log("Creating a new pool");
+    // poolInfo.push(
+    //   PoolInfo({
+    //     hardCap: pool.hardCap,
+    //     softCap: pool.softCap,
+    //     startDateTime: pool.startDateTime,
+    //     endDateTime: pool.endDateTime,
+    //     walletAddress: pool.walletAddress,
+    //     projectTokenAddress: pool.projectTokenAddress,
+    //     minAllocationPerUser: pool.minAllocationPerUser,
+    //     maxAllocationPerUser: pool.maxAllocationPerUser,
+    //     status: PoolStatus.Upcoming
+    //   })
+    // );
 
     return true;
   }
-  function addWhitelistUsers(address[] whitelistedAddresses) external onlyRole(POOL_OWNER_ROLE) {
+
+  function addWhitelistUsers(address[] calldata whitelistedAddresses)
+    external
+    onlyRole(POOL_OWNER_ROLE)
+  {
     addToWhitelist(whitelistedAddresses);
   }
+
   // **** </External-PoolOwner> ****
 
-
-
   // **** <Internal> ****
-  function getInvestorsAndTheirAllocations() internal returns (InvestorInfo[]){
-    return investorInfo; //TODO Iterate through it. needs Iterable.
-  }  
+  function getArrayOfInvestorsAllocation() internal {
+    // returns (InvestorInfo[] storage)
+    // // I think it is cost effectice to return count of investors and
+    // // webapp could use Promise.all(getInvestorInfo(index)); to fetch all invesors info concurrently.
+    // InvestorInfo[] storage investors;
+    // InvestorInfo[] storage investorsInfo = new InvestorInfo[](
+    //   countOfUsersWhitelisted
+    // );
+    // // the count of whitelisted users is kept in Whitelist.sol
+    // for (uint256 i = 0; i < countOfUsersWhitelisted; i++) {}
+    // return investorInfo; //TODO Iterate through it. needs Iterable.
+  }
+
   // **** </Internal> ****
 
-
-
   // **** <Private> ****
-  function isPoolOpen(PoolInfo _poolInfo) private view returns(bool) {
-    return (poolInfo[0].endDateTime < block.timestamp && poolInfo[0].status PoolStatus.Ongoing);
-  } 
   // **** </Private> ****
+
+  // **** <Modifier> ****
+  modifier pooIsOngoing() {
+    require(
+      poolInfo[0].status == PoolStatus.Ongoing &&
+        poolInfo[0].startDateTime >= block.timestamp &&
+        poolInfo[0].endDateTime <= block.timestamp
+    );
+    _;
+  }
+
+  // **** </Modifier> ****
 }
--- test/sample-test.ts
+++ test/sample-test.ts
@@ -3,50 +3,79 @@ const { ethers, Web3, web3 } = require("hardhat");
 import bigInt from "bignumber.js";
 
 let rewardTokenAddress = null;
+let idoContractAddress = null;
+let idoContract: any = null;
 
 function weiToDecimal(wei: bigInt) {
   return Web3.utils.fromWei(wei);
   // return ethers.utils.formatEther(wei);
 }
 
-describe("TKN", async () => {
-  it("Deploy <Reward Token>", async () => {
-    // Deploy Reward Token
-    const totalSupply = "10000";
-    const Token = await ethers.getContractFactory("RewardToken");
-    const token = await Token.deploy("Reward Token", "RT", totalSupply);
-    await token.deployed();
-
-    // Check total supply
-    let totalSupplyDeployed = weiToDecimal(
-      (await token.totalSupply()).toString()
-    );
-    expect(totalSupplyDeployed).be.equal(totalSupply);
-
-    const [owner, otherUser] = await ethers.getSigners();
-
-    // check balance of owner
-    const ownerBalance = weiToDecimal(
-      (await token.balanceOf(owner.address)).toString()
-    );
-    expect(ownerBalance).be.equal(totalSupply);
-
-    // check balance of one other user
-    const otherUserBalance = weiToDecimal(
-      (await token.balanceOf(otherUser.address)).toString()
-    );
-    expect(otherUserBalance).be.equal("0");
-
-    rewardTokenAddress = token.address;
-  });
-});
-
 describe("IDO", async () => {
-  // const [, second, third] = await ethers.getSigners();
+  it("deploy IDO contract", async () => {
+    //Deploy IDO
+    const IDO = await ethers.getContractFactory("VentIDO");
+    idoContract = await IDO.deploy();
+    await idoContract.deployed();
+    expect(idoContract.address.length).be.gt(0);
+  });
+
+  it("only poolOwner can create a pool", async () => {
+    const [, , otherUser] = await ethers.getSigners();
+
+    try {
+      await idoContract.createPool();
+    } catch (error) {
+      expect(true);
+    }
+  });
+
+  it("Grant poolOwner role to an address", async () => {
+    const [owner, poolOwner] = await ethers.getSigners();
+    await idoContract.grantPoolOwnerRole(poolOwner.address);
+  });
+
+  it("poolOwner can create a pool", async () => {
+    const [owner, poolOwner] = await ethers.getSigners();
+    await idoContract.connect(poolOwner.address).createPool();
+  });
+
+  it("revoke poolOwner access from an address", async () => {
+    const [, poolOwner] = await ethers.getSigners();
+    await idoContract.revokePoolOwnerRole(poolOwner.address);
+  });
+
   // const poolOwnerAddress = third.address;
 
   // const poolInfo = {};
 
   // const IDO = await ethers.getContractFactory("VentIDO");
   // const ido = await IDO.deploy();
+});
+
+describe("TKN", async () => {
+  // it("Deploy <Reward Token>", async () => {
+  //   // Deploy Reward Token
+  //   const totalSupply = "10000";
+  //   const Token = await ethers.getContractFactory("RewardToken");
+  //   const token = await Token.deploy("Reward Token", "RT", totalSupply);
+  //   await token.deployed();
+  //   // Check total supply
+  //   let totalSupplyDeployed = weiToDecimal(
+  //     (await token.totalSupply()).toString()
+  //   );
+  //   expect(totalSupplyDeployed).be.equal(totalSupply);
+  //   const [owner, otherUser] = await ethers.getSigners();
+  //   // check balance of owner
+  //   const ownerBalance = weiToDecimal(
+  //     (await token.balanceOf(owner.address)).toString()
+  //   );
+  //   expect(ownerBalance).be.equal(totalSupply);
+  //   // check balance of one other user
+  //   const otherUserBalance = weiToDecimal(
+  //     (await token.balanceOf(otherUser.address)).toString()
+  //   );
+  //   expect(otherUserBalance).be.equal("0");
+  //   rewardTokenAddress = token.address;
+  // });
 });
