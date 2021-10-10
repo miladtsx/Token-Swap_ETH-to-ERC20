c7faf383f859d889531f6faed62b6fd661c5e8c8
Milad Mehdizade <milad.mehdizade1394@gmail.com>
2021-10-10 16:45:20 +0330

    Pool Interface added

---
+++ contracts/IPool.sol
@@ -0,0 +1,51 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+interface IPool {
+  struct PoolModel {
+    uint256 hardCap; // how much project wants to raise
+    uint256 softCap; // how much of the raise will be accepted as successful IDO
+    uint256 startDateTime;
+    uint256 endDateTime;
+    address walletAddress; // address where Ether is sent
+    address projectTokenAddress; //the address of the token that project is offering in return
+    uint16 minAllocationPerUser;
+    uint256 maxAllocationPerUser;
+    PoolStatus status; //: by default “Upcoming”,
+    uint256 totalTokenProvided;
+    uint256 exchangeRate;
+    uint256 tokenPrice;
+    uint256 totalTokenSold;
+  }
+
+  struct ParticipantDetails {
+    address addressOfParticipant;
+    uint256 totalRaisedInWei;
+  }
+
+  // Pool data that needs to be retrieved:
+  struct PoolDetails {
+    ParticipantDetails[] investorsAndAllocations;
+    uint256 countOfInvestors;
+    uint256 totalRaised;
+    uint256 hardCap;
+    uint256 softCap;
+    uint16 minAllocationPerUser;
+    uint256 maxAllocationPerUser;
+    uint256 startDateTime;
+  }
+
+  enum PoolStatus {
+    Upcoming,
+    Cancelled,
+    Paused,
+    Finished,
+    Ongoing
+  }
+
+  function deposit() external payable returns (bool success);
+
+  function getPoolDetails() external view returns (PoolDetails memory);
+
+  // function withdraw(uint256 _amount) external;
+}
