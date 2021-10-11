8cc25c263b6122056bff80f410b2abf443befdd9
Milad Mehdizade <milad.mehdizade1394@gmail.com>
2021-10-11 10:54:40 +0330

    optimize deployed code

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -44,7 +44,13 @@ task(
  */
 module.exports = {
   solidity: {
-    compilers: [{ version: "0.8.0" }],
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
   },
   defaultNetwork: "localhost",
 };
