e706206d8e13e5e6390920824766ddd8dd5d9e84
Milad Mehdizade <milad.mehdizade1394@gmail.com>
2021-10-11 11:03:17 +0330

    reformat

--- contracts/VentIDO.sol
+++ contracts/VentIDO.sol
@@ -18,29 +18,77 @@ import "hardhat/console.sol"; //TODO debug
 
 contract VentIDO is Pausable, AccessControl, Whitelist, Ownable {
   bytes32 private constant POOL_OWNER_ROLE = keccak256("POOL_OWNER_ROLE");
-  Pool private pool;
+  IPool private pool;
 
   event LogPoolOwnerRoleGranted(address indexed owner);
   event LogPoolOwnerRoleRevoked(address indexed owner);
   event LogPoolCreated(address indexed poolOwner);
+  event LogPoolStatusChanged(address indexed poolOwner, uint256 newStatus);
 
   constructor() {
     _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
   }
 
-  function createPool(IPool.PoolModel calldata _poolInfo)
+  // Admin grants PoolOwner role to some address;
+  function grantPoolOwnerRole(address _address)
     external
-    payable
-    onlyRole(POOL_OWNER_ROLE)
+    onlyOwner
+    nonZeroAddress(_address)
   {
-    pool = new Pool(_poolInfo);
-    emit LogPoolCreated(_msgSender());
-    //TODO check if pool is created
+    grantRole(POOL_OWNER_ROLE, _address);
   }
 
-  function investInPool() external payable {
-    bool success = pool.deposit();
-    require(success, "Investing failed!");
+  // Admin revokes PoolOwner role feom an address;
+  function revokePoolOwnerRole(address _address)
+    external
+    onlyOwner
+    nonZeroAddress(_address)
+  {
+    revokeRole(POOL_OWNER_ROLE, _address);
+  }
+
+  function createPool(
+    uint256 _hardCap,
+    uint256 _softCap,
+    uint256 _startDateTime,
+    uint256 _endDateTime,
+    address _walletAddress
+  ) external payable onlyRole(POOL_OWNER_ROLE) returns (IPool) {
+    IPool.PoolModel memory model = IPool.PoolModel({
+      hardCap: _hardCap,
+      softCap: _softCap,
+      startDateTime: _startDateTime,
+      endDateTime: _endDateTime,
+      walletAddress: _walletAddress,
+      projectTokenAddress: _walletAddress, //TODO update this address when adding Token and withdrawal()
+      minAllocationPerUser: 1,
+      maxAllocationPerUser: 10000,
+      status: IPool.PoolStatus.Upcoming,
+      totalTokenProvided: 0,
+      exchangeRate: 1,
+      tokenPrice: 1,
+      totalTokenSold: 0
+    });
+
+    pool = new Pool(model);
+    emit LogPoolCreated(_msgSender());
+    return pool;
+  }
+
+  function getPoolDetails()
+    external
+    view
+    returns (IPool.PoolDetails memory poolDetails)
+  {
+    poolDetails = pool.getPoolDetails();
+  }
+
+  function updatePoolStatus(uint256 newStatus)
+    external
+    onlyRole(POOL_OWNER_ROLE)
+  {
+    pool.updatePoolStatus(newStatus);
+    emit LogPoolStatusChanged(_msgSender(), newStatus);
   }
 
   function addAddressesToWhitelist(address[] calldata whitelistedAddresses)
@@ -48,5 +96,10 @@ contract VentIDO is Pausable, AccessControl, Whitelist, Ownable {
     onlyRole(POOL_OWNER_ROLE)
   {
     addToWhitelist(whitelistedAddresses);
+  }
+
+  function investInPool() external payable {
+    bool success = pool.deposit();
+    require(success, "Investing failed!");
   }
 }
